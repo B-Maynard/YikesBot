32eb9332d1221ee3942a5e1863280e5a628af0bf
Bailey Maynard <bailey_maynard@live.com>
2019-05-12 20:24:14 -0400

    more testing

--- commands/memes/mesothelioma.js
+++ commands/memes/mesothelioma.js
@@ -27,7 +27,7 @@ class MesotheliomaCommand extends commando.Command {
     if (message.member.voiceChannel) {
       if (!message.guild.voiceConnection) {
         var connection = await message.member.voiceChannel.join();
-        Play(connection, message);
+        setTimeout(Play(connection, message), 5000);
       }
     }
   }
