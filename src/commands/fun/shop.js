e9f66d7bfe0713d955a0a5be4b9aa015df7e0d65
Bailey Maynard <bailey_maynard@live.com>
2025-09-27 18:42:21 -0400

    testing

--- src/commands/fun/shop.js
+++ src/commands/fun/shop.js
@@ -1,7 +1,7 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { StringSelectMenuOptionBuilder, MessageFlags, StringSelectMenuBuilder, ContainerBuilder, ActionRowBuilder, ButtonStyle, ButtonBuilder, SlashCommandBuilder } = require('discord.js');
 const shopUtil = require('../../helpers/shopHelpers');
 const { CurrencyShop, Users } = require('../../../db/currencyshop/dbObjects');
-const {codeBlock} = require('discord.js');
+const { codeBlock } = require('discord.js');
 
 const { Op } = require('sequelize');
 
@@ -11,34 +11,34 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('shop')
 		.setDescription('General shop commands')
-		.addSubcommand(sub => 
+		.addSubcommand(sub =>
 			sub.setName('balance')
 				.setDescription('Show a users balance')
-				.addUserOption(option => 
+				.addUserOption(option =>
 					option.setName('target')
 						.setDescription('A user to target')
 						.setRequired(false)
-			)
+				)
 		)
-		.addSubcommand(sub => 
+		.addSubcommand(sub =>
 			sub.setName('open')
 				.setDescription('Opens the shop')
 		)
-		.addSubcommand(sub => 
+		.addSubcommand(sub =>
 			sub.setName('leaderboard')
 				.setDescription('Opens the leaderboard')
 		)
-		.addSubcommand(sub => 
+		.addSubcommand(sub =>
 			sub.setName('buy')
 				.setDescription('Buy items from the shop')
-				.addStringOption(option => 
+				.addStringOption(option =>
 					option.setName('item')
-							.setDescription('The item to buy')
-							.setRequired(true)
+						.setDescription('The item to buy')
+						.setRequired(true)
 				)
 		),
 	async execute(interaction) {
-		
+
 		let user = interaction.options.getUser('target') || interaction.user;
 		let subCommand = interaction.options.getSubcommand();
 
@@ -47,7 +47,59 @@ module.exports = {
 				return interaction.reply(`${user.tag} has ${await shopUtil.getBalance(user.id)}💰`);
 			case "open":
 				const items = await CurrencyShop.findAll();
-				return interaction.reply(codeBlock(items.map(i => `${i.name}: ${i.cost}💰`).join('\n')));
+				let options = [];
+
+				items.forEach(item => {
+					options.push(new StringSelectMenuOptionBuilder()
+						.setLabel(item.dataValues.name)
+						.setDescription(`Cost: ${item.dataValues.cost}`)
+						.setValue(item.dataValues.id.toString()));
+				});
+
+				const exampleContainer = new ContainerBuilder()
+					.setAccentColor(0x0099FF)
+					.addActionRowComponents(
+						actionRow => actionRow
+							.setComponents(
+								new StringSelectMenuBuilder()
+									.setCustomId('shop')
+									.setPlaceholder('Choose item for purchase')
+									.addOptions(options)
+							),
+					)
+					.addSeparatorComponents(
+						separator => separator,
+					)
+					.addSectionComponents(
+						section => section
+							.setButtonAccessory(
+								button => button
+									.setCustomId('buyButton')
+									.setLabel('Purchase')
+									.setStyle(ButtonStyle.Primary),
+							),
+					);
+
+				const response = await interaction.reply({
+					components: [exampleContainer],
+					flags: MessageFlags.IsComponentsV2,
+					withResponse: true
+				});
+
+				const collectorFilter = i => i.user.id === interaction.user.id;
+				try {
+					const confirmation = await response.resource.message.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+					if (confirmation.customId === 'confirm') {
+						await confirmation.update({ content: `test`, components: [] });
+					} else if (confirmation.customId === 'cancel') {
+						await confirmation.update({ content: 'test2', components: [] });
+					}
+				} catch {
+					await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+				}
+				break;
+			// return interaction.reply(codeBlock(items.map(i => `${i.name}: ${i.cost}💰`).join('\n')));
 			case "leaderboard":
 				const users = await Users.findAll();
 				return interaction.reply(
@@ -66,7 +118,7 @@ module.exports = {
 					return interaction.reply("An item is required for this command.");
 				}
 
-				const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: itemString}}})
+				const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: itemString } } })
 
 				if (!item) return interaction.reply("Item does not exist in the shop.");
 
@@ -74,7 +126,7 @@ module.exports = {
 					return interaction.reply(`You currently have ${await shopUtil.getBalance(interaction.user.id)}💰, but ${item.name} costs ${item.cost}💰`);
 				}
 
-				const dbUser = await Users.findOne({where :{ user_id: interaction.user.id}});
+				const dbUser = await Users.findOne({ where: { user_id: interaction.user.id } });
 				shopUtil.addBalance(interaction.user.id, -item.cost);
 				await dbUser.addItem(item);
 
